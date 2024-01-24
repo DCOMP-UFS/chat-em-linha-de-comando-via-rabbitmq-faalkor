2a1029e2da83016bd06d456a3458c8fd33952a84
Ubuntu <ubuntu@ip-172-31-22-199.ec2.internal>
2024-01-24 23:46:04 +0000

    teste1

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -8,78 +8,48 @@ import java.util.Scanner;
 public class Chat {
 
   public static void main(String[] argv) throws Exception {
+    Scanner sc = new Scanner(System.in);
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("ip-da-instancia-da-aws"); // Alterar
-    factory.setUsername("usuário-do-rabbitmq-server"); // Alterar
-    factory.setPassword("senha-do-rabbitmq-server"); // Alterar
+    factory.setHost("54.226.237.127"); // Alterar
+    factory.setUsername("admin"); // Alterar
+    factory.setPassword("password"); // Alterar
     factory.setVirtualHost("/");
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
     
-    String QUEUE_NAME = "minha-fila";
+    //login usuario
+    System.out.print("User: ");
+    String user = sc.nextLine();
+    
                       //(queue-name, durable, exclusive, auto-delete, params); 
-    channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
+    channel.queueDeclare(user, false,   false,     false,       null);
     
     Consumer consumer = new DefaultConsumer(channel) {
       public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)           throws IOException {
-
+        
         String message = new String(body, "UTF-8");
         System.out.println(message);
 
       }
     };
                       //(queue-name, autoAck, consumer);    
-    channel.basicConsume(QUEUE_NAME, true,    consumer);
-    menu();
-  }
-  
-  
-  public static void menu() {
-    try {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("User: ");
-        String user = sc.nextLine();
-        login(user);
-        clearConsole();
-        while (true){
-          System.out.print(">> ");
-          String command = sc.nextLine();
-          if (command.startsWith("@")){
-            //chavear chat
-          }
+    channel.basicConsume(user, true, consumer);
+    
+    
+    // exibir prompt
+    while (true) {
+      System.out.print(">> ");
+      String command = sc.nextLine();
+      if (command.startsWith("@")){
+        //chavear chat
+      }
           
-          if(command.equalsIgnoreCase("exit")) break;           
-        }
-        sc.close();
-        
-    } 
-    catch (Exception e) {
-      System.out.println(e);
-    }
-  }
-  
-  
-  public final static void clearConsole() {
-    try {
-      final String os = System.getProperty("os.name");
-      if (os.contains("Windows")) {
-          new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      if(command.equalsIgnoreCase("exit")) break;           
       }
-      else {
-          Runtime.getRuntime().exec("clear");
-      }
-    }
-    catch (final Exception e) {
-      System.out.println(e);
-    }
+      sc.close();
   }
   
   
-  public static void login(String user) {
-    try {
-      // Criar fila do usuario no RabbitMQ 
-    } catch (Exception e) {
-        System.out.println(e);
-    }
-  }
+  
 }
+  
