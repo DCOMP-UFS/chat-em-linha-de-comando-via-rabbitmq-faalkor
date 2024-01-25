28189e875758c1b0c748c2f717cb1c03a1c11253
Ubuntu <ubuntu@ip-172-31-22-199.ec2.internal>
2024-01-25 02:39:21 +0000

    etapa 1

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -1,16 +1,17 @@
 package br.ufs.dcomp.ChatRabbitMQ;
-
 import com.rabbitmq.client.*;
-
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 
 public class Chat {
 
   public static void main(String[] argv) throws Exception {
-    Scanner sc = new Scanner(System.in);
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("54.226.237.127"); // Alterar
+    factory.setHost("54.196.154.240"); // Alterar
     factory.setUsername("admin"); // Alterar
     factory.setPassword("password"); // Alterar
     factory.setVirtualHost("/");
@@ -18,8 +19,10 @@ public class Chat {
     Channel channel = connection.createChannel();
     
     //login usuario
+    Scanner sc = new Scanner(System.in);
     System.out.print("User: ");
-    String user = sc.nextLine();
+    String user = sc.nextLine().trim();
+    String dest = "";
     
                       //(queue-name, durable, exclusive, auto-delete, params); 
     channel.queueDeclare(user, false,   false,     false,       null);
@@ -39,17 +42,40 @@ public class Chat {
     // exibir prompt
     while (true) {
       System.out.print(">> ");
-      String command = sc.nextLine();
-      if (command.startsWith("@")){
-        //chavear chat
+      dest = sc.nextLine().trim();
+      if (dest.startsWith("@")) {
+        dest = dest.substring(1);
+        break;
       }
-          
-      if(command.equalsIgnoreCase("exit")) break;           
+      else {
+        System.out.println("Informe o destinatário. Ex: @joao");
       }
+    }
+    
+    
+    while (true) {
+      System.out.print("@" + dest + ">> ");
+      String messege = sc.nextLine().trim();
+      if (messege.equalsIgnoreCase("exit")) break;
+      if (messege.startsWith("@")){
+        dest = messege.substring(1);
+      } 
+      else {
+        Date now = new Date();
+        SimpleDateFormat date_format = new SimpleDateFormat("dd/MM/yyyy 'às' HH:mm");
+        TimeZone timeZone = TimeZone.getTimeZone("America/Sao_Paulo");
+        date_format.setTimeZone(timeZone);
+        messege = "(" + date_format.format(now) + ") " + user + " diz: " + messege;
+        channel.basicPublish("", dest, null,  messege.getBytes("UTF-8"));
+      }
+      System.out.print("@" + dest + ">> ");
+    }
+    
       sc.close();
+      channel.queueDelete(user);
+      channel.close();
+      connection.close();
   }
-  
-  
   
 }
   
