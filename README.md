3cabe569475698480b0fbdc0b455f9de69985bd6
Ubuntu <ubuntu@ip-172-31-22-199.ec2.internal>
2024-02-08 13:58:19 +0000

    etapa 2

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Esta tarefa consiste no desenvolvimento de um cliente de chat do tipo linha de comando usando o RabbitMQ como servidor de mensagens de acordo com o apresentado em sala de aula.
 
 Ver demais arquivos das etapas do projeto.
+
+# Comandos para teste:
+mvn clean compile assembly:single
+java -jar target/ChatRabbitMQ-1.0-SNAPSHOT-jar-with-dependencies.jar
+
--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -6,26 +6,33 @@ import java.util.Scanner;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
-
+import com.google.protobuf.*;
+import java.nio.charset.StandardCharsets;
 
 
 public class Chat {
-    public static String dest;
+    public static String input;
+    public static String dest = "";
+    public static String destFormated;
+    public static String messege;
+    public static String user;
     public static byte[] buffer;
+    public static boolean flagGroup;
+    
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("34.224.61.130"); // Alterar
+    factory.setHost("18.208.211.193"); // Alterar
     factory.setUsername("admin"); // Alterar
     factory.setPassword("password"); // Alterar
     factory.setVirtualHost("/");
     Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
+    Channel channel = connection.createChannel(); //Criação de um novo canal
     
     //login usuario
     Scanner sc = new Scanner(System.in);
     System.out.print("User: ");
-    String user = sc.nextLine().trim();
+    user = sc.nextLine().trim();
     
     
                       //(queue-name, durable, exclusive, auto-delete, params); 
@@ -34,7 +41,7 @@ public class Chat {
     Consumer consumer = new DefaultConsumer(channel) {
       public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)           throws IOException {
         
-        ChatProto.Messege msg = ChatProto.Messege.parseFrom(buffer);
+        ChatProto.Messege msg = ChatProto.Messege.parseFrom(body);
         
         // Extraindo dados da mensagem
         String reciever = msg.getReciever();
@@ -45,74 +52,173 @@ public class Chat {
         
         ChatProto.Content content = msg.getContent();
         String type = content.getType();
-        String corp = content.getCorp();
+        byte[] corp = content.getCorp().toByteArray();
         String name = content.getName();
         
-        // Extraindo conteudo
-        //String message = new String(body, "UTF-8") ;
-        System.out.print("\n(" + date + ") " + sender + " diz: " + corp);
+        // Converter pra string -- depois filtar por tipo?
+        String txt = new String(corp, StandardCharsets.UTF_8);
+        
+        
+        // verificar se a msg foi em grupo
+        if (!group.equals("") && sender.equals(user)) return;
+        
+        System.out.print("\n(" + date +" às " + hour + ") " + sender + group + " diz: " + txt);
 
-        System.out.print("@" + dest + ">> ");
+        System.out.print("\n" + dest + ">> ");
       }
     };
                       //(queue-name, autoAck, consumer);    
     channel.basicConsume(user, true, consumer);
     
+    // welcome
+    //welcome();
     
     // exibir prompt
     while (true) {
-      System.out.print(">> ");
-      dest = sc.nextLine().trim();
-      if (dest.startsWith("@")) {
-        dest = dest.substring(1);
-        break;
-      }
-      else {
-        System.out.println("Informe o destinatário. Ex: @joao");
-      }
-    }
-    
-
-    while (true) {
-      System.out.print("@" + dest + ">> ");
-      String messege = sc.nextLine().trim();
-      if (messege.equalsIgnoreCase("exit")) break;
-      if (messege.startsWith("@")){
-        dest = messege.substring(1);
-      } 
-      else {
-        ChatProto.Messege.Builder bMsg = ChatProto.Messege.newBuilder();
-        ChatProto.Content.Builder content = ChatProto.Content.newBuilder();
-        
-        
-        Date now = new Date();
-        SimpleDateFormat date_format = new SimpleDateFormat("dd/MM/yyyy 'às' HH:mm");
-        TimeZone timeZone = TimeZone.getTimeZone("America/Sao_Paulo");
-        date_format.setTimeZone(timeZone);
-        
-        content.setType("");
-        content.setCorp(messege);
-        content.setName("");
-        
-        bMsg.setReciever(dest);
-        bMsg.setSender(user);
-        bMsg.setDate(date_format.format(now));
-        bMsg.setHour("");
-        bMsg.setGroup("");
-        bMsg.setContent(content);
-
-        ChatProto.Messege msg = bMsg.build();
-        buffer = msg.toByteArray();
+      System.out.print(dest + ">> ");
+      input = sc.nextLine().trim();
       
-        channel.basicPublish("", dest, null, buffer);
+      // Direct Messege
+      if (input.startsWith("@")) {
+        dest = input;
+        destFormated = destFormat(dest);
+        flagGroup = false;
+        continue;
+        //break;
+      }
+      
+      else if (input.startsWith("#")) {
+        dest = input;
+        destFormated = destFormat(dest);
+        flagGroup = true;
+        continue;
+        //break;
+      }
+      
+      // Group
+      else if (input.startsWith("!")) {
+        String[] promptArr = input.split(" ", -1);
         
+        switch (promptArr[0]) {
+            case "!addGroup":
+                //createGroup(promptArr[1]);
+                channel.exchangeDeclare(promptArr[1], "fanout");
+                channel.queueBind(user, promptArr[1], "");
+                break;
+                
+            case "!addUser":
+                channel.queueBind(promptArr[1], promptArr[2], "");
+                break;
+                
+            case "!delFromGroup":
+                channel.queueUnbind(promptArr[1], promptArr[2], "");
+                break;
+                
+            case "!removeGroup":
+                channel.exchangeDelete(promptArr[1]);
+                break;
+        }
+      }
+      
+      else if (input.equalsIgnoreCase("exit")) break;
+      
+      
+      else if (input.equals("")) System.out.println("Favor fornercer o input");
+      
+      else if (!input.equals("") && dest.equals("")) System.out.println("Favor informar o destinatário");
+      
+      else{
+        
+          try {
+              serializeMsg();
+          
+              // DM Msg
+              if (flagGroup == false) channel.basicPublish("", destFormated, null, buffer);
+                  
+              // Group Msg
+              else channel.basicPublish(destFormated, "", null, buffer);
+              
+          } 
+          catch (com.rabbitmq.client.AlreadyClosedException e) {
+                System.out.println("Usuario invalido\nFAVOR LOGAR NOVAMENTE");
+                break;
+                
+          }
+          catch (Exception e) {
+                System.out.println(e);
+                System.out.println("\n***Usuario invalido\nFAVOR LOGAR NOVAMENTE\n***");
+                break;
+          }
+          
       }
     }
     
+
       sc.close();
       channel.queueDelete(user);
       channel.close();
       connection.close();
+  }
+  
+  
+  public static void welcome() {
+     System.out.println("Welcome!");
+    /*
+    System.out.println("    ____ _           _     ____ \n" +            
+                       "/ ___| |__   __ _| |_  | __ )  _____  __ \n" +
+                       "| |   | '_ \ / _` | __| |  _ \ / _ \ \/ / \n" +
+                       "| |___| | | | (_| | |_  | |_) | (_) >  < \n"  + 
+                        "\____|_| |_|\__,_|\__| |____/ \___/_/\_\ \n"); 
+                        */
+                        
+  }
+  
+  
+  public static void createGroup(String groupName) {
+      //System.out.println(channel.exchangeDeclare(groupName, "fanout"));
+  }
+  
+  
+  public static String destFormat(String dest) {
+      return dest.substring(1);
+  }
+  
+  
+  public static void serializeMsg() {
+        try {
+            
+            ChatProto.Messege.Builder bMsg = ChatProto.Messege.newBuilder();
+            ChatProto.Content.Builder content = ChatProto.Content.newBuilder();
+           
+            Date now = new Date();
+            SimpleDateFormat date_format = new SimpleDateFormat("dd/MM/yyyy");
+            SimpleDateFormat hour = new SimpleDateFormat("HH:mm");
+            TimeZone timeZone = TimeZone.getTimeZone("America/Sao_Paulo");
+            
+            date_format.setTimeZone(timeZone);
+            hour.setTimeZone(timeZone);
+            
+            content.setType("");
+            content.setCorp(ByteString.copyFrom(input.getBytes("UTF-8")));
+            content.setName("");
+            
+            bMsg.setReciever(destFormated);
+            bMsg.setSender(user);
+            bMsg.setDate(date_format.format(now));
+            bMsg.setHour(hour.format(now));
+            
+            // Verify DM msg or Group msg
+            if (flagGroup == true) bMsg.setGroup(dest);
+            else bMsg.setGroup("");
+            
+            bMsg.setContent(content);
+    
+            ChatProto.Messege msg = bMsg.build();
+            buffer = msg.toByteArray();
+          
+        } catch(Exception e) {
+            System.out.println(e);
+        }
   }
   
 }
